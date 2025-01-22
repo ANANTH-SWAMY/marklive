f9be98ea7bc40799404119ba99febf4a8798e17c
ANANTH-SWAMY <ananth.gnr@gmail.com>
2025-01-22 22:48:29 +0530

    error handling

--- index.js
+++ index.js
@@ -12,19 +12,31 @@ const { Server } = require("socket.io")
 
 let args = minimist(process.argv.slice(2))
 
-const PORT = 7000
+if (args["port"] == true) {
+	console.log(
+		"\n",
+		chalk.bgRedBright.bold.black(" ERROR ")
+		,"Missing port number",
+		"\n"
+	)
+
+	process.exit(1)
+}
+
+const PORT = args["port"] || 7000
 
 if (args["_"].length > 1) {
 	console.log(
 		"\n",
-		chalk.bgRedBright.bold.black(" ERR ")
+		chalk.bgRedBright.bold.black(" ERROR ")
 		,"More than one file given",
 		"\n"
 	)
+
 	process.exit(1)
 }
 
-if (args["_"].length == 0) {
+if (args["_"].length === 0) {
 	process.exit(1)
 }
 
@@ -67,11 +79,23 @@ server.listen(PORT, () => {
 		chalk.cyan("http://localhost:") + chalk.cyan.bold(`${PORT}`),
 		"\n"
 	)
+}).on("error", (err) => {
+
+	if (err.errno === -13) {
+		console.log(
+			"\n",
+			chalk.bgRedBright.bold.black(" ERROR ")
+			,"Permission denied",
+			"\n"
+		)
+	}
+
+	process.exit(1)
 })
 
 // help
-// title
 // port
 // highlight
 // markdownit-checkbox
 // markdownit-emoji
+// pdf
