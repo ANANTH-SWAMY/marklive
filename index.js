ff5110871cfb04d00b0e43e6a793b55ca5a6d0e1
ANANTH-SWAMY <ananth.gnr@gmail.com>
2025-01-22 12:27:34 +0530

    add package-lock.json to gitignore

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 #! /usr/bin/env node
 
 const chalk = require("chalk")
+const chokidar = require("chokidar")
+const express = require("express")
+const fs = require("fs")
+const http = require("http")
 const markdownit = require("markdown-it")
 const minimist = require("minimist")
 
-const PORT = 7000
-
 let args = minimist(process.argv.slice(2))
+
+const PORT = 7000
 
 if (args["_"].length > 1) {
 	console.log(chalk.red("More than one file given"))
 	process.exit(1)
 }
 
-if (!args["_"][0].endsWith(".md")) {
-	console.log(chalk.red("Not a Markdown file"))
-	process.exit(1)
-}
+const filepath = __dirname + "/" + args["_"][0]
+
+const watcher = chokidar.watch(filepath)
+
+watcher.on("change", async () => {
+	console.log("change")
+})
+
+// let file = fs.readFileSync(filepath).toString()
+// console.log(markdownit().render(file))
